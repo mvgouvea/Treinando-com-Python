ba58256ddaaf820cfaea11ca24b701a1dd3b7284
Marcos Gouvea <marcoskilf@gmail.com>
2022-07-13 23:24:25 -0300

    aula 15 - concacentando strings

--- eXcript - Python/aula13.py
+++ eXcript - Python/aula13.py
@@ -1,12 +1,18 @@
 num_int = 5
-num_dec = 7.3
-val_str = "Três formas de concaternar String"
+num_dec = 7.3591
+val_str = "qualquer texto"
 
-print(val_str)
+print("Três formas de concaternar String")
 #Concactenar um valor inteiro com uma String utiliza virgula
 print("o valor é:", num_int)
 #Dentro da string, se coloca um marcador, utilizando o simbolo do tipo correspondente a variavel %i ou  %f
 print("o valor é: %i" %num_int)
-print("Tipo real: %f" %num_dec)
+#para contrar a quantidade de casas decimais, basta colocar %. e a quantidade seguido do f
+print("Tipo real: %.3f" %num_dec)
 #concactena ambos valores string com sinal de adição, em seguida utiliza o cast para conversão.
 print("o valor é: "+ str(num_int))
+#para concactena é necessário ambos ser do mesmo tipo
+print("#-------------------#")
+print("Concactenando strings", val_str)
+print("Concactenando strings %s" %val_str)
+print("Concactenando strings "+ val_str)
