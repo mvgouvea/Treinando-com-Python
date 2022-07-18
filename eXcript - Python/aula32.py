998a8cff89d7a254bc8f3688d8a7f50ce8689c61
Marcos Gouvea <marcoskilf@gmail.com>
2022-07-18 19:52:26 -0300

    Atribuição composta
    Atalhos matemáticos, operadores compostos, que são formados por mais de um operador e assim, além de atribuir um valor, ainda realizar uma outra função.

---
+++ eXcript - Python/aula32.py
@@ -0,0 +1,36 @@
+print("Atalhos matemáticos")
+"""
+ATRIBUIÇÃO SIMPLES
+= - operador de atribuição simples
+
+ATRIBUIÇÃO COMPOSTA
++= - mais igual
+-= - menos igual
+*= - vezes igual
+/= - dividido igual
+%= - módulo igual
+"""
+a=10
+b=20
+print("O valor é atribuido da direita para a esquerda")
+print("O valor de 'a' é: %i e o 'b' é: %i" %(a,b))
+a+=b
+print("A expressão é 'a+=b' =",a)
+print()
+a=10
+b=20
+print("O valor de 'a' é: %i e o 'b' é: %i" %(a,b))
+a-=b
+print("A expressão é 'a-=b' =",a)
+print()
+a=10
+b=20
+print("O valor de 'a' é: %i e o 'b' é: %i" %(a,b))
+a*=b
+print("A expressão é 'a*=b' =",a)
+print()
+a=10
+b=20
+print("O valor de 'a' é: %i e o 'b' é: %i" %(a,b))
+a/=b
+print("A expressão é 'a/=b' =",a)
