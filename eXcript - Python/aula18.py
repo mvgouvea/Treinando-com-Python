71049086a22e1cc66949352ca868d665b62ac62a
Marcos Gouvea <marcoskilf@gmail.com>
2022-07-18 10:32:51 -0300

    Radiação e Potencialização
    Utilizando operador ** composto e importando biblioteca math

---
+++ eXcript - Python/aula18.py
@@ -0,0 +1,25 @@
+#importanto biblioteca
+import math
+#sem necessidade de colocar número em loop para pegar a potência
+print("---------POTÊNCIA---------")
+#Utilizando o simbolo ** significa que queremos a potencia do número a esquerda, elevado
+print("Operador composto **")
+print("A potencia do número a esquerda, elevado ao número a direita")
+print("Expressão: 5**2")
+print("Resultado:",5**2)
+print()
+print("A raiz de um número, corresponde a 1 dividido pelo expoente")
+print("A operação realiza o cálculo de 10 elevado ao cubo. ", 10**3)
+""" O operador de potenciação em Python é um operador binário, logo, 
+sempre precisaremos informar dois valores, no caso, o operador e a 
+potência, que o Python no retornara o número correspondente. """
+print()
+print("---------RADICIAÇÃO---------")
+print("Expressão: 9 ** (1/2)")
+print("O resultado da expressão será", 9 ** (1/2))
+print()
+print("Assim, temos um operador binário tanto para a potenciação, como também, a radiciação. ")
+print()
+print("-------Biblioteca Math-------")
+#após importar a biblioteca e quiser saber 
+print("Resultado de PI:", math.pi)
