89cf25f732cc8eeffbf87a7903cf9f553b16e27e
Marcos Gouvea <marcoskilf@gmail.com>
2022-07-18 19:31:45 -0300

    atribuição múltipla

---
+++ eXcript - Python/aula31.py
@@ -0,0 +1,9 @@
+print("Atribuição Mutipla")
+#Enquando outras linguagens, é necessário criar uma terceira várivavel
+a=10
+b=20
+a, b = b, a
+s1, s2 = "curso", "python"
+print(a, b)
+print("%s de %s" %(s1, s2))
+#Pode atribuir operações matemáticas
