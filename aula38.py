cd97a60e57bdb41805aa7a5abf2245aa55eaaf94
Marcos Gouvea <marcoskilf@gmail.com>
2022-07-27 16:35:35 -0300

    correção de diretório

--- aula38.py
+++
@@ -1,7 +0,0 @@
-#Função range
-#na versão 3.0 é necessário converter o range para uma lista
-
-#o número 2 tem a função de definir o passo da função
-print(list(range(0,10,2)))
-#função simplificado como padrão o passo é um
-print(list(range(10)))
