f1a5888e8512c4e5342274b6dfc368e442dad3f2
Marcos Gouvea <marcoskilf@gmail.com>
2022-07-27 09:52:00 -0300

    Estrtutura de atribuição condicional Demonstra como simplificar o código com duas ou mais condições.

---
+++ eXcript - Python/aula33.py
@@ -0,0 +1,9 @@
+
+num1 = int(input("Digite um número inteiro: "))
+if (num1 % 2 == 0):
+    s = "par"
+else:
+    s = "impar"
+#Estrtutura de atribuição condicional
+#s = "par" if num1 % 2 == 0 else "impar"
+print("O número digitado é:", s)
