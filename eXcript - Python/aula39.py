60c0fa4c4037c4750c5660d9e294cb5e30b58f70
Marcos Gouvea <marcoskilf@gmail.com>
2022-10-10 18:00:42 -0300

    Estrutura for Descrevendo inicio, parada e passada

---
+++ eXcript - Python/aula39.py
@@ -0,0 +1,10 @@
+start = 0
+stop = 10
+step = 2
+#variavel i recebe a lista range com 10 valores
+for i in range(4):
+    print(i)
+
+print("-------------")
+for f in range(start, stop, step):
+    print(f)
